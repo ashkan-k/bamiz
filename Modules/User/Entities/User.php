a9909869d57e52abc688e4d8c84f83fc7c5567df
ashkan karimi <karimiashkan8186@gmail.com>
2023-05-29 20:16:55 +0330

    init project (install dependencies and packages)

---
+++ Modules/User/Entities/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Modules\User\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'username',
+//        'block_status',
+        'level',
+        'phone',
+        'avatar',
+        'email_verified_at'
+    ];
+
+    protected static function newFactory()
+    {
+        return \Modules\User\Database\factories\UserFactory::new();
+    }
+
+    public function isAdmin()
+    {
+        return $this->level == 'admin';
+    }
+
+    //
+
+//    public function articles()
+//    {
+//        return $this->hasMany(Article::class);
+//    }
+//
+//    public function centers()
+//    {
+//        return $this->hasMany(Center::class);
+//    }
+//
+//    public function galleries()
+//    {
+//        return $this->hasMany(Gallery::class);
+//    }
+//
+//    public function comments()
+//    {
+//        return $this->hasMany(Comment::class);
+//    }
+//
+//    public function wish_lists()
+//    {
+//        return $this->hasMany(WishList::class);
+//    }
+//
+//    public function reserves()
+//    {
+//        return $this->hasMany(Reserve::class);
+//    }
+//
+//    public function tickets()
+//    {
+//        return $this->hasMany(Ticket::class);
+//    }
+//
+//    public function answers()
+//    {
+//        return $this->hasMany(Answer::class);
+//    }
+//
+//    public function payments()
+//    {
+//        return $this->hasMany(Payment::class);
+//    }
+}
---
+++ app/Enums/EnumHelpers.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Enums;
+
+
+class EnumHelpers
+{
+    static $UserLevelEnum = ['user', 'staff', 'admin'];
+}
