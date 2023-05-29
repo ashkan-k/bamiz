30e77b8bf0fc66d97217afa459cedb4e7feaac9c
ashkan karimi <karimiashkan8186@gmail.com>
2023-05-29 23:05:09 +0330

    add admin panel theme and initial it

---
+++ Modules/Dashboard/Http/Livewire/Pages/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Dashboard\Http\Livewire\Pages;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public function render()
+    {
+        return view('dashboard::livewire.pages.dashboard');
+    }
+}
