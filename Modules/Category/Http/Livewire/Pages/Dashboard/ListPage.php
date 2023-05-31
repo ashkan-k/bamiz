d35c3b7ea0a7f0deffbb0aaebdd06cd927f99185
ashkan karimi <karimiashkan8186@gmail.com>
2023-05-31 19:28:00 +0330

    add categories crud

---
+++ Modules/Category/Http/Livewire/Pages/Dashboard/ListPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Category\Http\Livewire\Pages\Dashboard;
+
+use Livewire\Component;
+
+class ListPage extends Component
+{
+    public function render()
+    {
+        return view('category::livewire.pages.dashboard.list-page');
+    }
+}
