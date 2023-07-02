1e52dcc0940fcabcae393e1975ca8f48a216ca67
ashkan karimi <karimiashkan8186@gmail.com>
2023-07-02 18:15:01 +0330

    complete reserve page

--- Modules/Payment/Routes/front.php
+++ Modules/Payment/Routes/front.php
@@ -5,6 +5,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Payment\Http\Controllers\Front\FrontPaymentController;
 
 // پرداخت و درگاه پرداخت
-Route::get('pay' , [FrontPaymentController::class , 'show']);
-Route::post('payment/{reserve}' , [FrontPaymentController::class , 'pay'])->name('payment')->middleware('auth');
-Route::get('payment/callback' , [FrontPaymentController::class , 'call_back'])->name('callback')->middleware('auth');
+Route::post('payment/{reserve}', [FrontPaymentController::class, 'pay'])->name('payment')->middleware('auth');
+Route::get('payment/callback', [FrontPaymentController::class, 'call_back'])->name('callback')->middleware('auth');
