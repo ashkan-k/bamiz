161fad228ccfbca4e9db776da2c8114e9333a468
ashkan karimi <karimiashkan8186@gmail.com>
2023-07-16 01:03:35 +0330

    ***

--- resources/views/test_map.blade.php
+++ resources/views/test_map.blade.php
@@ -1,66 +1,122 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-          integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
-          integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <link
+        rel="stylesheet"
+        href="https://www.parsimap.com/docs/leaflet/v1.5.1/leaflet.css"
+    />
+    <script src="https://www.parsimap.com/docs/leaflet/v1.5.1/leaflet.js"></script>
 
     <style>
-        #owl-demo .item{
-            margin: 3px;
+        @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700');
+        * {
+            margin:0;
         }
-        #owl-demo .item img{
-            display: block;
-            width: 100%;
-            height: auto;
+        * + * {
+            margin-top:10px;
+        }
+        body {
+            background-color:#eee;
+            margin:0;
+        }
+        body,button,input {
+            font-family:'IBM Plex Sans',sans-serif;
+            font-size:1.2rem;
+            line-height:1.5;
+        }
+        button,input {
+            background-color:#eee;
+            border:1px #999 solid;
+            border-radius:4px;
+            cursor:pointer;
+            padding:5px 15px;
+            transition:all 250ms;
+        }
+        form {
+            padding:40px;
+        }
+        label {
+            display:block;
+        }
+        #map {
+        * + * {
+            margin:0;
+        }
+        }
+
+        body {
+            margin: 0;
+        }
+
+        html,
+        body,
+        #map {
+            height: 100%;
         }
     </style>
-
 </head>
 <body>
+<div id="map"></div>
 
-<div id="owl-demo" class="owl-carousel owl-theme">
-
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-    <div class="item"><img src="http://127.0.0.1:8000/uploads/places_covers/2023-7-3/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D8%AE%D9%88%D8%B1%D8%B4%DB%8C%D8%AF/600_NrL5sUhkUupsSO4HfU3A-1688381881.5322.jpg" alt="Owl Image"></div>
-
-</div>
-
-
-<script type="text/javascript" src="/admin/assets/js/jquery-3.2.1.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
-        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
+<form>
+    <div><label for="lat">Latitude</label><input type="text" name="lat" id="lat"></div>
+    <div><label for="lng">Longitude</label><input type="text" name="lng" id="lng"></div>
+</form>
 
 <script>
-    $(document).ready(function() {
+    var map;
+    var pin;
+    var tilesURL='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
+    var mapAttrib='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>';
+    var marker;
 
-        $("#owl-demo").owlCarousel();
+    // add map container
+    // $('body').prepend('<div id="map" style="height:70vh;width:100%;"></div>');
+    MapCreate();
 
+    function MapCreate() {
+        // create map instance
+        if (!(typeof map == "object")) {
+            map = L.map('map', {
+                center: [40,0],
+                zoom: 3
+            }).setView([29.6760859,52.4950737], 13);
+        }
+        else {
+            map.setZoom(3).panTo([40,0]);
+        }
+        // create the tile layer with correct attribution
+        L.tileLayer(tilesURL, {
+            attribution: mapAttrib,
+            maxZoom: 19
+        }).addTo(map);
+
+        marker = L.marker([29.623116,52.497856], { riseOnHover:true,draggable:true }).addTo(map);
+    }
+
+    map.on('click', function(ev) {
+        $('#lat').val(ev.latlng.lat);
+        $('#lng').val(ev.latlng.lng);
+
+        marker.remove();
+
+        if (typeof pin == "object") {
+            pin.setLatLng(ev.latlng);
+        }
+        else {
+            pin = L.marker(ev.latlng,{ riseOnHover:true,draggable:true });
+            pin.addTo(map);
+            pin.on('drag',function(ev) {
+                $('#lat').val(ev.latlng.lat);
+                $('#lng').val(ev.latlng.lng);
+            });
+        }
     });
 </script>
-
 </body>
 </html>
