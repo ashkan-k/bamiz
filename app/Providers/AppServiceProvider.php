300766600e4ea088df634094e6b381aadef9299f
ashkan karimi <karimiashkan8186@gmail.com>
2023-07-21 15:34:44 +0330

    ***

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (!env('APP_DEBUG')){
+            $this->app->bind('path.public', function() {
+                return realpath(base_path().'/public_html');
+            });
+        }
     }
 
     /**
