3365db10d4b4a59e342975c7182063fe31d2f57d
ashkan_k <karimiashkan8181@gmail.com>
2023-08-13 01:42:13 +0330

    ***

--- Modules/Place/Routes/front.php
+++ Modules/Place/Routes/front.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Place\Http\Controllers\Front\FrontPlaceController;
 
 Route::get('places' , [FrontPlaceController::class , 'places'])->name('places');
-Route::get('places/{place:slug}' , [FrontPlaceController::class , 'place_detail'])->name('place_detail');
+Route::get('{place:slug}' , [FrontPlaceController::class , 'place_detail'])->name('place_detail');
 Route::get('places/categories/{category:slug}' , [FrontPlaceController::class , 'categories'])->name('categories');
